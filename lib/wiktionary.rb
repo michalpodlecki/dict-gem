428f1987b0886caa70c9f5f8303db3e08c0589cc
kdunee <kosmadunikowski@gmail.com>
2012-07-04 17:33:32 +0200

    [#32253677] underscores are added in wikipedia queries

--- lib/wiktionary.rb
+++ lib/wiktionary.rb
@@ -5,27 +5,29 @@ class Wiktionary
   WIKI_URL = "http://en.wiktionary.org/wiki/"
   def initialize(word)
     if word.empty? then raise ArgumentError, "No word given." end
-    @uri = URI(URI.escape(WIKI_URL + word.downcase))
+    escaped_word = word.downcase.tr(' ', '_')
+    @uri = URI(URI.escape(WIKI_URL + escaped_word))
   end
-  
+
   def translate
     req = Net::HTTP::Get.new(@uri.path)
     response, translations, sentences = nil, [], []
     Net::HTTP.start(@uri.host, @uri.port) do |http|
       response = http.request(req).body
-      
+
       doc = Nokogiri::HTML(response)
       doc.css('div#mw-content-text[lang=en] ol > li a').each do |link|
         translations.push link.content
       end
-      
+
       translations.each do |item|
-        sentence = Nokogiri::HTML(Net::HTTP.get(URI(WIKI_URL + item)))
+        escaped_item = item.tr(' ', '_')
+        sentence = Nokogiri::HTML(Net::HTTP.get(URI(WIKI_URL + escaped_item)))
         sentence.css('div#mw-content-text[lang=en] ol > li dl dd i').each do |s|
           sentences.push s.content
         end
       end
-      
+
     end
     [translations, sentences]
   end
