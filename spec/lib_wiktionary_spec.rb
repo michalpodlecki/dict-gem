6aace7bc76ad4421cf0e6f72f28ff004b6db2105
Mateusz Czerwiński <mrfustrr@gmail.com>
2012-07-05 13:25:14 +0200

    style

--- spec/lib_wiktionary_spec.rb
+++ spec/lib_wiktionary_spec.rb
@@ -6,38 +6,37 @@ describe Wiktionary do
     expect { Wiktionary.new }.to raise_error ArgumentError
   end
 
-	it "should return an empty array" do
-  	word = "dziwneslowo"
-		wiki = Wiktionary.new(word)
-		wiki.translate.should be_a(Array)
-		wiki.translate[0].size.should == 0
-	end
+  it "should return an empty array" do
+    word = "dziwneslowo"
+    wiki = Wiktionary.new(word)
+    wiki.translate.should be_a(Array)
+    wiki.translate[0].size.should == 0
+  end
 	
-	it "should return an one element array, containing =war= string" do
-  	word = "wojna"
-		wiki = Wiktionary.new(word)
+  it "should return an one element array, containing =war= string" do
+    word = "wojna"
+    wiki = Wiktionary.new(word)
 		
-		wiki.translate.should be_a(Array)
-
-		wiki.translate[0][0].should eq("war")
-	end
+    wiki.translate.should be_a(Array)
+    wiki.translate[0][0].should eq("war")
+  end
 	
-	it "should return an two element array of translations containing [\"car\",\"automobile\"]" do
-		word = "samochód"
-		wiki = Wiktionary.new word
-		wiki.translate[0].size.should == 2
-		wiki.translate[0].should eq(["car","automobile"])
-	end
-	
-	it "should return a proper translations for 'car' word" do
-	  word = "samochód"
-	  wiki = Wiktionary.new(word)
-	  wiki.translate[1][0].should eq("She drove her car to the mall.")
-	  wiki.translate[1][1].should eq("The conductor linked the cars to the locomotive.")
-	end
-	
-	it "should raise an error because of not polish word" do
-	  wiki = Wiktionary.new("field")
-	  expect { wiki.translate }.to raise_error("Given word is not polish.")
-	end
+  it "should return an two element array of translations containing [\"car\",\"automobile\"]" do
+    word = "samochód"
+    wiki = Wiktionary.new word
+    wiki.translate[0].size.should == 2
+    wiki.translate[0].should eq(["car","automobile"])
+  end
+  
+  it "should return a proper translations for 'car' word" do
+    word = "samochód"
+    wiki = Wiktionary.new(word)
+    wiki.translate[1][0].should eq("She drove her car to the mall.")
+    wiki.translate[1][1].should eq("The conductor linked the cars to the locomotive.")
+  end
+  
+  it "should raise an error because of not polish word" do
+    wiki = Wiktionary.new("field")
+    expect { wiki.translate }.to raise_error("Given word is not polish.")
+  end
 end
