37abd8042d0182c83fd58523901d771eaad26125
Aleksander Gozdek <aleksander.gozdek@o2.pl>
2012-07-05 14:19:59 +0200

    [#32241465] added rspec test for script dictpl returning translations in many contexts

--- spec/lib_dictpl_spec.rb
+++ spec/lib_dictpl_spec.rb
@@ -1,5 +1,5 @@
 # -*- encoding: utf-8 -*
-require_relative '../dictpl'
+require_relative '../lib/dictpl'
 
 describe Dictpl do
   
@@ -7,18 +7,20 @@ describe Dictpl do
     expect { Dictpl.new }.to raise_error ArgumentError
   end
   
-  it "should return string 'cow'" do
-    word = "krowa"
+  it "should return hash for 'krowa'" do
+    word = "krowa"    
     dict = Dictpl.new(word)
-    dict.translate.should be_a(String)
-    dict.translate.should eq("cow")
+    result = dict.translate
+    result.translations.should be_a(Hash)
+    result.translations.should == {"krowa"=>["cow"], "krowa bliska wycielenia"=>["freshen of cow"], "krowa mleczna"=>["milker", "milcher", "milk cow", "dairy cow", "milch cow"], "krowa morska; Hydrodamalis gigas"=>["Steller's sea cow"], "krowa morska; krowa morska Stellera; Hydrodamalis gigas (wymarły ssak morski)"=>["Steller's sea cow"], "krowa nie zapłodniona"=>["barren cow"], "krowa po pierwszym ocieleniu się"=>["cow heifer"], "krowa wysokomleczna"=>["deep milking cow"], "cielna krowa"=>["springer"], "dojna krowa"=>["milch-cow"], "mleczna krowa"=>["milk cow"], "wata cukrowa"=>["candyfloss", "candy floss", "candy-floss", "cotton candy"], "mąka cukrowa"=>["false grain"], "święta krowa"=>["sacred moose"], "trzcina cukrowa"=>["sugarcane"], "lukier; polewa lukrowa"=>["icing"], "święta krowa [przen]"=>["sacred cow"]} 
   end
   
-  it "should return string 'hi'" do
-    word = "cześć"
+  it "should return hash for 'car'" do
+    word = "car"    
     dict = Dictpl.new(word)
-    dict.translate.should be_a(String)
-    dict.translate.should eq("hi")
+    result = dict.translate
+    result.translations.should be_a(Hash)
+    result.translations.should == {"car"=>["tsar", "tzar"], "Caracas"=>["Caracas"], "Caranx bartholomaei (gatunek ryby)"=>["yellow jack"], "Caranx caballus (gatunek ryby)"=>["green jack"], "Caranx caninus (gatunek ryby)"=>["Pacific crevalle jack"], "Caranx crysos (gatunek ryby)"=>["blue runner"], "Caranx heberi (gatunek ryby)"=>["blacktip trevally"], "Caranx hippos (gatunek ryby)"=>["cavalla", "Crevalle jack"], "Caranx ignobilis (gatunek ryby)"=>["giant trevally"], "Caranx kleinii (gatunek ryby)"=>["razorbelly scad"], "samochód"=>["car"], "wagon tramwajowy (rodzaj pojazdu transportu publicznego)"=>["car"], "antena samochodowa (do radia samochodowego)"=>["car aerial", "car antenna"], "samochód-pułapka"=>["car bomb"], "drobna, okazyjna sprzedaż (często prosto z bagażnika samochodu)"=>["car boot sale"], "samochodziarz (osoba pasjonująca się naprawianiem lub odnawianiem samochodów)"=>["car buff"], "zegar samochodowy (czasowy)"=>["car clock"], "włamanie do samochodu"=>["car clout"], "prochowiec"=>["car coat"], "kraksa samochodowa"=>["car crash"], "dealer samochodowy; salon samochodowy"=>["car dealer"]}
   end  
   
 end
