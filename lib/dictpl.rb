5ececd3abdda470bae78960f7978a709ef98dcfe
Aleksander Gozdek <aleksander.gozdek@o2.pl>
2012-07-06 23:58:55 +0200

    [#32348655] first simple refactoring of dictpl

--- lib/dictpl.rb
+++ lib/dictpl.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'open-uri'
 require 'nokogiri'
 require_relative 'result'
@@ -6,11 +7,9 @@ require_relative 'result'
 class Dictpl
 
   DICT_URL = "http://dict.pl/dict?word=" 
-  def initialize(word)
-    raise ArgumentError, "No given word" if word.empty?
-    @word = word
-    @uri = URI(URI.escape(DICT_URL + @word + "&lang=EN"))	
-    @result = Result.new(word)
+  def initialize(word)    
+		check_arguments(word)
+		initialize_instance_arguments(word)
   end
   
   #
@@ -31,10 +30,22 @@ class Dictpl
     @context_words.each_slice(2) do |word|	     
       @mapped_words[word.first] = word.last
       @result.add_translation(word.first, word.last)
-    end
-    
+    end    
     @result
   end
-	  
+
+	def check_arguments(word)												
+    if word.empty? then raise ArgumentError.new("No word given.") end
+  end
+	
+	def initialize_instance_arguments(word)
+		@word = word
+    @uri = URI(URI.escape(DICT_URL + @word + "&lang=EN"))	
+    @result = Result.new(word)
+  end
 end
 
+a = Dictpl.new "samochód"
+a.translate.translations.each_pair do |key, value|
+  puts key + value.to_s
+end
