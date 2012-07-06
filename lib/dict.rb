3b8d7aa22a3839afc57f43e1c9da9380150bd16e
Rafał <rafal.osko@gmail.com>
2012-07-06 14:09:24 +0200

    [#32353299] new module implementation

--- lib/dict.rb
+++ lib/dict.rb
@@ -1,34 +1,33 @@
 #!/usr/bin/ruby -w
 # -*- coding: utf-8 -*-
 
-require 'uri'
-require 'net/http'
-require 'timeout'
+require_relative 'wiktionary'
+require_relative 'dictpl'
 
 module Dict
-  class Translation
-    def initialize(word)
-        @word = word#ARGV[0]
+  class << self
+    def get_all_dictionaries_translations(word)
+      dictionaries = Hash.new
+      
+      available_services.each do |service|
+        dictionaries[service] = get_single_dictionary_translations(word, service)
+      end
+      
+      dictionaries
     end
     
-    def self.status(adres = "http://dict-app-staging.shellyapp.com/")
-      uri = URI(adres)
-      res = Net::HTTP.get_response(uri)
-      res.message
-    end
-
-    def self.get_response(word, time, dict)
-      uri = URI.parse(URI.escape("http://dict-app-staging.shellyapp.com/#{dict}/#{word}"))
-      begin
-        Timeout::timeout(time.to_i) do
-          res = Net::HTTP.get(uri)
-          "Success" unless res.empty?
-        end
-        
-        rescue #=> ex
-          puts "Timeout for the query."
-          {:code => 408, :describe => "Request Timeout"}
+    def get_single_dictionary_translations(word, service)
+      case service
+      when 'wiktionary'
+        Wiktionary.new(word).translate
+      when 'dictpl'
+        Dictpl.new(word).translate
+      else 'There\'s no such dictionary.'
       end
+    end
+    
+    def available_services
+      ['wiktionary', 'dictpl']
     end
   end
 end
