7ef35c717afd7087c1f744da32c4659e78ac7236
Mateusz Czerwiński <mrfustrr@gmail.com>
2012-07-06 13:24:20 +0200

    [#32283363] refactoring

--- lib/wiktionary.rb
+++ lib/wiktionary.rb
@@ -17,6 +17,7 @@
 
 require 'net/http'
 require 'nokogiri'
+
 require_relative 'result'
 
 class Wiktionary
@@ -27,35 +28,36 @@ class Wiktionary
   end
   
   def translate
-    req = Net::HTTP::Get.new(@uri.path)
-    response, translations = nil, []
-    Net::HTTP.start(@uri.host, @uri.port) do |http|
-      response = http.request(req).body
-      
-      doc = Nokogiri::HTML(response)
+    translations = []
+    
+    doc = Nokogiri::HTML(Net::HTTP.get_response(@uri).body)
 
-      return @result unless is_polish?(doc)
-
-      doc.css('div#mw-content-text[lang=en] ol > li a').each do |link|
-        @result.add_translation(@result.term,link.content)
-      end
-      
-      doc.css('div#mw-content-text[lang=en] ol > li a').each do |link|
-        translations.push(link.content)
-      end
-
-      translations.each do |item|
-        escaped_item = item.tr(' ', '_')
-        example = Nokogiri::HTML(Net::HTTP.get(URI(WIKI_URL + escaped_item)))
-        example.css('div#mw-content-text[lang=en] ol:first > li dl dd i').each do |s|
-          @result.add_example(@result.term,s.content)
-        end
-      end
+    return @result unless is_polish?(doc)
+    
+    doc.css('div#mw-content-text[lang=en] ol > li a').each do |link|
+      @result.add_translation(@result.term, link.content)
     end
+      
+    
+    doc.css('div#mw-content-text[lang=en] ol > li a').each do |link|
+      translations.push(link.content)
+    end
+
+    examples_of_translations(@result, translations)
     
     @result
   end
   
+  private
+  def examples_of_translations(result, translations)
+    translations.each do |item|
+      example = Nokogiri::HTML(Net::HTTP.get(URI(WIKI_URL + item.tr(' ', '_'))))
+      example.css('div#mw-content-text[lang=en] ol:first > li dl dd i').each do |s|
+        result.add_example(result.term, s.content)
+      end
+    end
+  end
+
   def initialize_instance_arguments(word)
     escaped_word = word.downcase.tr(' ', '_')
     @result = Result.new(escaped_word)
@@ -67,9 +69,9 @@ class Wiktionary
   end
 
   def is_polish?(doc)
-    polish = doc.css('div#mw-content-text h2 .mw-headline').any? do |lang|
+    doc.css('div#mw-content-text h2 .mw-headline').any? do |lang|
       lang.content == 'Polish'
     end
   end
-  
+ 
 end
