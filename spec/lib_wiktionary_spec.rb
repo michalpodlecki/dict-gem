0c581fcdfe2bdd94aab011e9ebac9f551cee6fee
Kosma Dunikowski <kosmadunikowski@gmail.com>
2012-07-09 12:57:40 +0200

    [#32253677] test for replacement of spaces with underscores in multi-word wiktionary queries

--- spec/lib_wiktionary_spec.rb
+++ spec/lib_wiktionary_spec.rb
@@ -5,7 +5,7 @@ describe Wiktionary do
   it "should be an argument given" do
     expect { Wiktionary.new }.to raise_error ArgumentError
   end
-  
+
   it "should return an empty translations array" do
     word = "dziwneslowo"
     wiki = Wiktionary.new(word)
@@ -13,15 +13,15 @@ describe Wiktionary do
     result.translations.should be_a(Hash)
     result.translations.size.should == 0
   end
-  
+
   it "should return an one element array, containing =war= string" do
     word = "wojna"
     wiki = Wiktionary.new(word)
-    
+
     wiki.translate.translations[word].should be_a(Array)
     wiki.translate.translations[word].first.should eq("war")
   end
-  
+
   it "should return an two element array of translations containing [\"car\",\"automobile\"]" do
     word = "samochód"
     wiki = Wiktionary.new(word).translate
@@ -29,13 +29,21 @@ describe Wiktionary do
     result[word].size.should == 2
     result[word].should eq(["car","automobile"])
   end
-  
+
   it "should return a proper translations for 'car' word" do
     word = "samochód"
     wiki = Wiktionary.new(word)
     result = wiki.translate
     result.examples[word][0].should eq("She drove her car to the mall.")
     result.examples[word][1].should eq("The conductor linked the cars to the locomotive.")
+  end
+
+  it "should replace spaces with underscores in multi-word queries" do
+    word = "podwyższać się"
+    wiki = Wiktionary.new(word)
+    result = wiki.translate
+    expected_term = "podwyższać_się"
+    result.term.should eq expected_term
   end
 
 end
