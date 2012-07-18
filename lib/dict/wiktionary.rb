c24bbe2315fe9bc0e95bac4d85941ba83f77d922
Kosma Dunikowski <kosmadunikowski@gmail.com>
2012-07-18 11:52:10 +0200

    removed obsolote url in wiktionary

--- lib/dict/wiktionary.rb
+++ lib/dict/wiktionary.rb
@@ -3,24 +3,22 @@
 require 'nokogiri'
 require 'dict/dictionary'
 
-WIKI_URL = 'http://en.wiktionary.org/wiki/'
-
 module Dict
   class Wiktionary < Dictionary
-  
+
     # Returns an Dict::Result object
 	  def translate
       translations.each { |item| @result.add_translation(@result.term, item) }
-      
+
       @result
-    end 
-    
+    end
+
     def get_html(url)
       Nokogiri::HTML(open(URI.encode(url)))
     rescue OpenURI::HTTPError
       raise Dictionary::ConnectError
     end
-    
+
     private
     def polish?(content)
       ! /==Polish==/i.match(content).nil?
@@ -30,7 +28,7 @@ module Dict
     def translations
       url_pl = "http://en.wiktionary.org/w/index.php?title=#{@word}&action=edit"
       url_en = "http://pl.wiktionary.org/w/index.php?title=#{@word}&action=edit"
-      
+
       content_pl = get_html(url_pl).css('textarea#wpTextbox1').first
       if polish?(content_pl)
         extract_polish_translations(content_pl)
@@ -38,13 +36,13 @@ module Dict
         extract_english_translations(get_html(url_en).css('textarea#wpTextbox1').first.content)
       end
     end
-    
+
     # Returns an array containing polish translations.
     def extract_polish_translations(content)
       translations = /Noun[^\{]+\{\{(?:head\|pl|pl\-noun)[^#]+#\s*\[\[([^\n]+)/.match(content)
       translations = (translations && translations[1].gsub(/\[|\]/,'').split(', ')) || []
     end
-    
+
     # Returns an array containing english translations.
     def extract_english_translations(content)
       translations_block = /język\s+angielski(?:.|\n)+\{\{znaczenia\}\}(.|\n)+(?:\{\{odmiana){1,}/.match(content)
@@ -57,7 +55,7 @@ module Dict
       translations.delete_if(&:empty?)
       translations ||= []
     end
-    
+
     def get_examples(content, polish = true)
       # todo
     end
